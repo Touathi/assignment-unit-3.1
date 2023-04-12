2e2208f356a48848315d05d26e08aa9afe838c1c
Touathi Vang <Touathi@outlook.com>
2023-04-12 11:47:21 -0500

    Added favorite food array and console logged it and the length, also console logged the 2nd animal of the animal array

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -9,11 +9,12 @@ console.log('Animals are: ', animalArray);
 
 // 1.a. TODO: Make an array with some favorite foods
 
+let favoriteFoodsArray = ['sphagetti', 'lasagna', 'steak', 'pho'];
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar 
 //      to the example above
 
-
+console.log('Favorite foods are: ', favoriteFoodsArray);
 
 // 2. Array.length - tells you how many items are in the array
 console.log('--- 2. Length of an array ---');
@@ -23,15 +24,17 @@ console.log('Number of animals:', animalArray.length);
 
 // 2.a. TODO: Log to the console the number of foods in your array
 
+console.log('Number of favorite foods:', favoriteFoodsArray.length);
 
 // 3. Accessing array items
 console.log('--- 3. Accessing items in an array ---');
 
 // Example: Log the first animal from the array using it's array index
-console.log('First animal is', animalArray[0])
+console.log('First animal is', animalArray[0]);
 
 // 3.a. TODO: Log the second animal in the array 
 
+console.log('Second favorite food is', animalArray[1]);
 
 // 3.b. TODO: Log the last animal in the array using it's array index 
 
